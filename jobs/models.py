7dc8358fcafdb97ef26adbcc22fa8cbf9148cbad
Hardik <hardikzurdo>
2019-04-03 15:17:13 -0400

    updated models

--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to = 'images/')
-    summary = models.CharField(max_length =200)
-    
+    title = models.CharField(max_length = 50)
+    company = models.CharField(max_length = 50)
+    summary = models.TextField()
